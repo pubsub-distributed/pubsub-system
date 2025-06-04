b6a98bd8b1ca4972ee94ff8db3cc086be7f73a08
Shijian Huang <wongszekim@gmail.com>
2025-06-04 03:58:00 -0700

    Update scripts

--- scripts/show_subscribers.py
+++ scripts/show_subscribers.py
@@ -1,13 +1,21 @@
 import json
 import requests
+import socket
 
-with open("peers.json", "r") as f:
+
+with open("../peers.json", "r") as f:
     peers = json.load(f)
+
+my_ips = ["127.0.0.1", "localhost"]
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
 
 # Map: topic -> [list of node_ids]
 topic_to_nodes = {}
 
 for node_id, (ip, port) in peers.items():
+    if ip == local_ip:
+        ip = "127.0.0.1"
     try:
         url = f"http://{ip}:{port}/status"
         resp = requests.get(url, timeout=3)
